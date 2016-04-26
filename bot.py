cb1c47b2693451415d2007a57f1ba13d7d24063c
neraserpent <albaserpent@gmail.com>
2016-04-26 03:45:39 +0300

    Добавлен предварительный вариант поиска сериала по названию - функция give_me. Для корректной работы требуется установка IMDbPY. Требуется обсудить весь алгоритм на примере use-case перед написанием последующего кода и проверить на запущенном боте.

--- bot.py
+++ bot.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
+from imdb import IMDb
+ia = IMDb()
+
+def give_me(series_name):
+    movie_list = ia.search_movie(series_name)
+        if len(movie_list) > 0:
+            bot.send_message(m.chat.id, 'Which one?')
+                for movie_number in range(len(movie_list)):
+                    bot.send_message(m.chat.id,
+                                     str(movie_number) + ': ' +
+                                     movie_list[movie_number].data['title'])
+                    #функция с дальнейшими действиями
+                else:
+                    bot.send_message(m.chat.id, 'Sorry, i\'ve found nothing.')
 
 def listener(messages):
     for m in messages:
         if m.content_type == 'text':
-            bot.send_message(m.chat.id,m.text)
+            if m.text[0:7] == u'give me':
+                series_name = m.text[8:]
+                give_me(series_name)
+            #дальнейшие варианты исходной команды
+            #elif m.text[] == u''                  
+            bot.send_message(m.chat.id, m.text)
 
             
 if __name__ == '__main__' :
